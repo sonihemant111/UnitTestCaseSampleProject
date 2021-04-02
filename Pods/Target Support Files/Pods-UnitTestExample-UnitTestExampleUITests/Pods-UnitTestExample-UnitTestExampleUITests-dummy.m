c6ffa28ecffed3f59be2f99ec4d101495cf1a1ec
Hemant Soni <sonihemant111@gmail.com>
2021-04-02 11:40:42 +0530

    Added Appcenter

---
+++ Pods/Target Support Files/Pods-UnitTestExample-UnitTestExampleUITests/Pods-UnitTestExample-UnitTestExampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UnitTestExample_UnitTestExampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_UnitTestExample_UnitTestExampleUITests
+@end
---
+++ Pods/Target Support Files/Pods-UnitTestExample-UnitTestExampleUITests/Pods-UnitTestExample-UnitTestExampleUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_UnitTestExample_UnitTestExampleUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_UnitTestExample_UnitTestExampleUITestsVersionString[];
+
